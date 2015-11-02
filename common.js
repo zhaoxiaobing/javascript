29eef2db9320ff1503c3ddf9cadd225bf94c0b23
Zhao Xiao Bing <378164732@qq.com>
2015-11-02 19:44:06 +0800

    test

--- common.js
+++ common.js
@@ -1,3 +1,11 @@
 /**
  * Created by lenovo on 2015/11/2.
  */
+
+function getStyle(obj, attr){
+    if(obj.currentStyle){//ie
+        return obj.currentStyle[attr];
+    }else{//ff,chrome,ie8+
+        return getComputedStyle(obj,false)[attr];
+    }
+}
